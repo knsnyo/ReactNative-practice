d2a892d309ac50bab28fa04a6bb5520c34e421b8
knsnyo <isc963@naver.com>
2022-07-16 19:43:14 +0900

    20220716 why not stop

--- alarm/App.js
+++ alarm/App.js
@@ -1,12 +1,25 @@
 import React from "react";
 import { StatusBar, } from "expo-status-bar";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Button } from "react-native";
 import { BUTTON_HEIGHT, VIEW_WIDTH } from "./values";
 import TimePicker from "./components/TimePicker/TimePicker";
 import { asPickerFormat } from "./utils";
 
 export default () => {
   const [time, setTime] = React.useState(asPickerFormat(new Date()));
+  const alarm = React.useRef(null);
+
+  const alarmHandler = _ => {
+    console.log("HI");
+    let currentTime;
+    alarm.current = setInterval(_ => {
+      currentTime = new Date();
+      console.log(`time: ${time.toTimeString().slice(0, 5)}`);
+      console.log(`currentTime: ${currentTime.toTimeString().slice(0, 5)}`);
+      console.log(time.toTimeString().slice(0, 5) == asPickerFormat(currentTime).toTimeString().slice(0, 5));
+      if(currentTime == time) clearInterval(alarm.current);
+    }, 1000);
+  };
 
   return (
     <View style={styles.view}>
@@ -18,7 +31,8 @@ export default () => {
         buttonHeight={BUTTON_HEIGHT}
         visibleCount={3}
       />
-      <StatusBar style="auto" />
+      <Button title="alarm" onPress={alarmHandler}/>
+      <StatusBar style="auto"/>
     </View>
   );
 };
